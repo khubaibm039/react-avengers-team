99ab03bed0ff672f81c69d69c17cb6f44c7a24b4
khubaib004 <khabbab607342@gmail.com>
2023-09-16 16:29:38 +0600

    add sweet alert

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -3,10 +3,15 @@ import "./home.css"
 import { useState } from "react";
 import Cart from "../Cart/Cart";
 import Card from "../Card/Card";
+import Swal from 'sweetalert2/src/sweetalert2.js'
+
+
+
 const Home = () => {
     const [carts, setCarts] = useState([])
     const [selectedActors, setSelectedActors] = useState([])
     const [totalAmount, setTotalAmount] = useState(0)
+    const [remaining, setTotalRemaining] = useState(20000)
     useEffect(() => {
         fetch('./data.json')
             .then(res => res.json())
@@ -15,11 +20,43 @@ const Home = () => {
 
     const handleSlectActor = (actor) => {
         // console.log(actor);
-        const actorSelected = [...selectedActors, actor]
-        setSelectedActors(actorSelected)
-        setTotalAmount(totalAmount + actor.salary)
+        const isExits = selectedActors.find(id => actor.name === id.name);
+        if (!isExits) {
+            const actorSelected = [...selectedActors, actor]
+            const totalAmountRemaining = remaining - actor.salary
 
-    }
+            if (totalAmountRemaining < 0) {
+                return (
+                    Swal.fire({
+                        icon: 'error',
+                        title: 'Oops...',
+                        text: 'Something went wrong!',
+                        footer: '<a href="">Why do I have this issue?</a>',
+                        timer: 200
+                    })
+                )
+            }
+
+            setSelectedActors(actorSelected)
+            setTotalRemaining(totalAmountRemaining);
+            setTotalAmount(totalAmount + actor.salary)
+
+
+
+        }
+        else {
+            Swal.fire({
+                icon: 'error',
+                title: 'Oops...',
+                text: 'Something went wrong!',
+                footer: '<a href="">Why do I have this issue?</a>',
+                timer: 2000
+            })
+        }
+
+
+    };
+
 
     return (
         <div className="container">
@@ -30,8 +67,10 @@ const Home = () => {
                     }
                 </div>
                 <div>
-                    <h1>this is cart</h1>
+                    <h1>This is cart</h1>
                     <h2>Total Amount : {totalAmount}</h2>
+                    <h2>Remaining : {remaining}</h2>
+                    <h3>Total Actor : {selectedActors.length}</h3>
                     {
                         selectedActors.map((actor, idx) => <Card actor={actor} key={idx}></Card>)
                     }
